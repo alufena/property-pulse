bad5da87cd2115b1d7fb7ba8714ce822fa31b017
Anderson <andersonpolv@gmail.com>
2025-05-01 15:32:08 -0300

    new file:   app/layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+// aqui é o entrypoint do projeto
+
+import '@/assets/styles/globals.css'; // @ significa AT, ou seja, raiz do diretório
+
+export const metadata = {
+  // as páginas estão server side (react components no servidor), estão sendo renderizadas no server. abaixo são keywords para SEO. fazer isso aqui (layout.jsx) afeta todas as páginas do projeto
+  title: 'Impulso Imobiliário encontre o seu aluguel perfeito', // influencia a aba do navegador
+  description: 'Encontre o imóvel dos seus sonhos para alugar',
+  keywords:
+    'rental, find rentals, find properties, aluguel, encontrar aluguéis, encontrar propriedades,',
+};
+
+const MainLayout = ({
+  children, // as páginas serão exibidas no layout através de uma prop passadas aqui como children
+}) => {
+  return (
+    // envoltado em parênteses para retornar tag html
+    <html lang="en">
+      <body>
+        {/* <div>Main Layout</div> */}
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+// pasta "app" será tratada como páginas dos componentes e api routes
+
+// export const metadata = {
+//   title: 'Test', // agora só a "home page" tem esse título, ao invés de usar só o que está definido globalmente em layout
+// };
+
+const HomePage = () => {
+  return <div>Home Page</div>;
+};
+
+export default HomePage;
