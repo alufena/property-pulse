d94115085fe2bfbd3ca8e1178d81b1d46cd929de
Anderson <andersonpolv@gmail.com>
2025-05-02 14:11:39 -0300

    modified:   app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
-// pasta "app" será tratada como páginas dos componentes e api routes. o uso de "server components" é o padrão
+// pasta "app" será tratada como páginas dos componentes e api routes. o uso de "server components" é o padrão e aqui é um desse tipo
 
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI); // acessar variáveis .ENV é por intermédio de "process.env."
   return (
     <>
       <Hero />
