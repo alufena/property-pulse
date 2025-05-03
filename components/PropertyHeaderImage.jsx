68a934a216f89b64424efc3b2f3cf0705badc33a
Anderson <andersonpolv@gmail.com>
2025-05-03 15:26:50 -0300

    new file:   components/Spinner.jsx

---
+++ components/PropertyHeaderImage.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+const PropertyHeaderImage = ({ image }) => {
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <Image
+            // src="images/properties/a1.jpg"
+            src={`/images/properties/${image}`}
+            alt=""
+            className="object-cover h-[400px] w-full" // "width", "height", "sizes" e "priority" com esses valores e o "w-full" daqui consertam o erro "missing required "height" property" dentre outro warning
+            width={0}
+            height={0}
+            sizes="100vw"
+            priority={true}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PropertyHeaderImage;
